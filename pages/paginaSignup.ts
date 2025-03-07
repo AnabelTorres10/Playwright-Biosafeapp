45950c9671c32f788d029c52d28a8264a411e09b
Peolahh <bautiyluis@gmail.com>
2025-03-07 10:13:53 -0300

    Refactorizacion de pagina de signup

--- pages/paginaSignup.ts
+++ pages/paginaSignup.ts
@@ -1,6 +1,14 @@
 import { Page, Locator, expect } from "@playwright/test"
+import { getVerificationCode } from '../utils/gmailUtils';
+import PaginaVerificacionEmail from '../pages/PaginaVerificacionEmail';
+import PaginaLogin from './PaginaLogin';
+import data from '../data/usuarios.json'
+
+
 
 export default class paginaSignup {
+    readonly paginaVerificacionEmail: PaginaVerificacionEmail;
+    readonly paginaLogin: PaginaLogin;
     readonly page: Page;
     readonly nombreInput: Locator;
     readonly emailInput: Locator;
@@ -11,16 +19,19 @@ export default class paginaSignup {
 
     constructor(page: Page) {
         this.page = page;
+        this.paginaVerificacionEmail = new PaginaVerificacionEmail(page);
+        this.paginaLogin = new PaginaLogin(page);
         this.nombreInput = page.getByTestId('nameInput');
         this.emailInput = page.getByTestId('emailInput');
         this.contrasenaInput = page.getByTestId('passwordInput');
         this.confirmarContrasenaInput = page.getByTestId('confirmPasswordInput');
         this.botonDeRegistrarse = page.getByTestId('botonRegistro');
         this.registroexitosoAlert = page.getByText('¡Registro exitoso!');
+        
     }
 
-    async completarRegistroExitoso(user: any) {
-        
+
+    async userSignupAndLogin(user: any) {
         const mailDeUsuarioUnico = user.email.replace('@', "+" + Date.now() + "@");
         console.log(mailDeUsuarioUnico);
         await this.nombreInput.fill(user.nombre);
@@ -29,9 +40,24 @@ export default class paginaSignup {
         await this.confirmarContrasenaInput.fill(user.contrasena);
         await this.botonDeRegistrarse.click();
         await expect(this.registroexitosoAlert).toBeVisible();
-        return mailDeUsuarioUnico;
+
+        await expect(this.page).toHaveURL(process.env.BASE_URL! + '/verify-email')
+        await this.page.waitForTimeout(2000)
+        const verificationCode = await getVerificationCode()
+
+        console.log("Código de verificación: ", verificationCode)
+        if (verificationCode) {
+        await this.paginaVerificacionEmail.codigoVerificacionInput.fill(verificationCode)
+        await this.paginaVerificacionEmail.verificarButton.click()
+        await expect(this.paginaVerificacionEmail.verificacionExitosaAlert).toBeVisible({ timeout: 10000 })
+        await expect(this.page).toHaveURL(process.env.BASE_URL! + '/login')
+        await this.paginaLogin.emailInput.fill(mailDeUsuarioUnico)
+        await this.paginaLogin.passwordInput.fill(user.contrasena)
+        await this.paginaLogin.loginButton.click();
+        await expect(this.page).toHaveURL(process.env.BASE_URL! + '/dashboard')
+
+
     }
 
-
-
+}
 }
